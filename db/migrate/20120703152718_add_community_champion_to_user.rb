2456a8ef99b7cad560e0ae0021e68cbce382588e
Edward Andrews <edward@yoomee.com>
2012-07-04 10:59:56 +0100

    Add champion_request_at to User

--- db/migrate/20120703152718_add_community_champion_to_user.rb
+++ db/migrate/20120703152718_add_community_champion_to_user.rb
@@ -2,8 +2,10 @@ class AddCommunityChampionToUser < ActiveRecord::Migration
   def change
     add_column :users, :is_community_champion, :boolean
     add_column :users, :community_champion_id, :integer
+    add_column :users, :champion_request_at, :datetime
     
     add_index :users, :community_champion_id
+    add_index :users, :champion_request_at
   end
   
 end
