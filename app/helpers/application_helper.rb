870d35b6777b2d983d2b85cdd44439edfe3f3a38
Ian Mooney <ian@yoomee.com>
2012-07-05 09:22:18 +0100

    Display 'No messages' instead of '0 Messages'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,18 +12,18 @@ module ApplicationHelper
   def messages_badge(message_count)
     if message_count > 0
       out = content_tag(:span, message_count, :class => 'badge')
-      out + content_tag(:span, " Message#{message_count > 1 ? 's' : ''}", :class => "request-message-text")
+      out + content_tag(:span, " message#{message_count > 1 ? 's' : ''}", :class => "request-message-text")
     else
-      content_tag(:span, "0 Messages", :class => "request-message-text")
+      content_tag(:span, "No messages", :class => "request-message-text")
     end
   end
   
   def header_messages_badge(message_count, options = {})
     if message_count > 0
       out = content_tag(:span, message_count, :class => 'badge badge-info')
-      out + " Message#{message_count > 1 ? 's' : ''}"
+      out + " message#{message_count > 1 ? 's' : ''}"
     else
-      "0 Messages"
+      "No messages"
     end
   end
   
