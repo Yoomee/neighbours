e7d093f05a1cd0acaef20cb3ab57d259269e63b4
Ian Mooney <ian@yoomee.com>
2012-07-05 16:02:04 +0100

    Create enquiry and enquiry_field tables

---
+++ db/migrate/20120705135928_create_enquiries.rb
@@ -0,0 +1,8 @@
+class CreateEnquiries < ActiveRecord::Migration
+  def change
+    create_table :enquiries do |t|
+      t.string :form_name
+      t.timestamps
+    end    
+  end
+end
---
+++ db/migrate/20120705140151_enquiry_field.rb
@@ -0,0 +1,10 @@
+class EnquiryField < ActiveRecord::Migration
+  def change
+    create_table :enquiry_fields do |t|
+      t.integer :enquiry_id
+      t.string :name
+      t.text :value
+      t.timestamps
+    end
+  end
+end
