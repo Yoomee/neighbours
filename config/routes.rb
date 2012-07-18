c54e83ffe0b98653dd386d4cd1435a368bb961aa
Edward Andrews <edward@yoomee.com>
2012-07-18 17:49:44 +0100

    Force user to finish registration if incomplete

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Neighbours::Application.routes.draw do
   resources :wireframes, :only => [:index, :show]
   resources :registrations, :only => [:new, :create]  
   match "registrations/where_you_live" => "registrations#new", :step => "where_you_live", :as => "where_you_live_registration"
+  match "registrations/validate" => "registrations#new", :step => "validate", :as => "validate_registration"
   
   resources :needs do
     resources :offers, :only => [:create]
