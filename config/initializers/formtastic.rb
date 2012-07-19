6d546a60357baa2fad0ea69f1dd31a6378b7eb35
Matt Atkins <matt@yoomee.com>
2012-07-19 15:27:28 +0100

    Allow single quotes in tooltip help

--- config/initializers/formtastic.rb
+++ config/initializers/formtastic.rb
@@ -4,7 +4,7 @@ module FormtasticBootstrap::Inputs::Base
   
   def help_html
     return "" if options[:help].blank?
-    "<i class='icon-question-sign help-icon' title='#{options[:help]}'></i>".html_safe
+    "<i class='icon-question-sign help-icon' title='#{options[:help].gsub(/'/,"&#39;")}'></i>".html_safe
   end  
   
 end
