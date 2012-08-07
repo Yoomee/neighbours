d63c96ec312d4be987397ed6109ea4de2a2b0a58
Matt Atkins <matt@yoomee.com>
2012-08-07 12:41:50 +0100

    Uncomment important part of migration!

--- db/migrate/20120807105730_add_validation_code_to_users.rb
+++ db/migrate/20120807105730_add_validation_code_to_users.rb
@@ -1,6 +1,7 @@
 class AddValidationCodeToUsers < ActiveRecord::Migration
   def up
-    #add_column :users, :validation_code, :string, :after => "requested_validation_by_credit_card"
+    add_column :users, :validation_code, :string, :after => "requested_validation_by_credit_card"
+    User.reset_column_information
     User.all.each do |user|
       user.send(:generate_validation_code)
       user.save
