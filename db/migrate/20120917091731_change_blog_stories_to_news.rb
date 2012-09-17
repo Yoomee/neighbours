923598a95c083386b3b0ee00a1be28ebb71079a3
Edward Andrews <edward@yoomee.com>
2012-09-17 10:45:42 +0100

    Amalgamate blog, stories to news

---
+++ db/migrate/20120917091731_change_blog_stories_to_news.rb
@@ -0,0 +1,12 @@
+class ChangeBlogStoriesToNews < ActiveRecord::Migration
+  def up
+    news =  Page.find_by_slug(:news) || Page.create(:slug => 'news', :title => 'News', :layout => 'list')
+    pages = Page.find_by_slug(:blog).try(:children) +  Page.find_by_slug(:stories).try(:children)
+    pages.flatten.compact.each do |page|
+      page.update_attribute(:parent, news)
+    end
+  end
+  
+  def down
+  end
+end
