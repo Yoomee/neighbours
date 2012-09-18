8f071378be745fc1dec2a46c87bdc04e3e524808
Edward Andrews <edward@yoomee.com>
2012-09-18 08:38:21 +0100

    Correct view_name in migration

--- db/migrate/20120917091731_change_blog_stories_to_news.rb
+++ db/migrate/20120917091731_change_blog_stories_to_news.rb
@@ -1,6 +1,6 @@
 class ChangeBlogStoriesToNews < ActiveRecord::Migration
   def up
-    news =  Page.find_by_slug(:news) || Page.create(:slug => 'news', :title => 'News', :layout => 'list')
+    news =  Page.find_by_slug(:news) || Page.create(:slug => 'news', :title => 'News', :view_name => 'list')
     pages = Page.find_by_slug(:blog).try(:children) +  Page.find_by_slug(:stories).try(:children)
     pages.flatten.compact.each do |page|
       page.update_attribute(:parent, news)
