1e8bc2627d73ebe861742f54086013d78d96ab36
Andy Mayer <andy@yoomee.com>
2012-12-02 12:44:35 +0000

    Show deadline date on needs page

--- app/helpers/need_helper.rb
+++ app/helpers/need_helper.rb
@@ -3,5 +3,13 @@ module NeedHelper
   def need_radius_options
     logged_in? ? current_user.radius_options : Need.radius_options.select { |k,v| v <= AreaRadiusMaximum::DEFAULT_MAXIMUM.to_i }
   end
+  
+  def show_deadline(need)
+    if need.need_to_know_by == 'date'
+      "by #{need.deadline}"
+    else
+      need.need_to_know_by.to_s(:date)
+    end
+  end
 
 end
