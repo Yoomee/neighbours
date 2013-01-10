3d33ed424e7c5dd09b1394755615a7976d8e8add
Andy Mayer <andy@yoomee.com>
2013-01-10 10:17:38 +0000

    Show real neighbourhoods on the "not in your area" page

--- app/models/neighbourhood.rb
+++ app/models/neighbourhood.rb
@@ -5,5 +5,13 @@ class Neighbourhood < ActiveRecord::Base
   has_many :area_radius_maximums, :dependent => :destroy
   
   accepts_nested_attributes_for :area_radius_maximums, :reject_if => :all_blank, :allow_destroy => true 
+  
+  def status
+    if live then
+      "Live"
+    else
+      "Coming soon"
+    end
+  end
     
 end
