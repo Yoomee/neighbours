f89c1f64da22635e01b560bb17baddf8f6d52e83
Andy Mayer <andy@yoomee.com>
2013-01-18 01:10:31 +0000

    Share on Facebook and Twitter

--- app/helpers/share_helper.rb
+++ app/helpers/share_helper.rb
@@ -5,17 +5,16 @@ module ShareHelper
       :app_id => Settings.facebook_app_id,
       :link => request.url,
       :redirect_uri => request.url,
-      :name => "Name",
+      :name => "We have users in your area, but need more before we can launch",
       :description => strip_tags(options[:share_text].presence || object.text).truncate(150)
     }
-    # if object.image.present?
-    #   share_params[:picture] = "#{Settings.site_url}/#{object.image.thumb("400x").url}"
-    # end
-    link_to(content_tag(:i, '', :class => 'icon-facebook-sign'), "https://www.facebook.com/dialog/feed?#{share_params.to_query}", :target => "_blank", :rel => 'tooltip', :id => "facebook_share_link", :data => {:title => "Share on Facebook", :placement => 'left'})
+    # share_params[:picture] = "#{Settings.site_url}/assets/person.png"
+    
+    link_to(content_tag("i", "Share on Facebook", :class => 'icon-facebook'), "https://www.facebook.com/dialog/feed?#{share_params.to_query}", :target => "_blank", :rel => 'tooltip', :id => "facebook_share_link", :data => {:title => "Share on Facebook", :placement => 'left'})
   end
 
   def twitter_share()
-    link_to(content_tag(:i, '', :class => 'icon-twitter-sign'), "https://twitter.com/intent/tweet?url=#{request.url}", :target => "_blank", :rel => 'tooltip', :id => "twitter_share_link", :data => {:title => "Share on Twitter", :placement => 'left'})
+    link_to(content_tag(:i, '', :class => 'icon-twitter-sign'), "https://twitter.com/intent/tweet?url=#{request.url}&text=abc", :target => "_blank", :rel => 'tooltip', :id => "twitter_share_link", :data => {:title => "Share on Twitter", :placement => 'left'})
   end
 
 end
