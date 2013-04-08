b0a859546b69f273dce72fa70a2f930d08c3879d
Ian Mooney <ian@yoomee.com>
2013-04-08 17:15:57 +0100

    Refactor registration flow

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -26,27 +26,19 @@ class RegistrationsController < ApplicationController
         if @user.last_step?
           if @user.save
             sign_in(@user)
-            if true
-              if @user.validate_by == "post" && !@user.validated?
-                flash[:modal] = {:title => "Thanks for registering", :text => "We'll send you a letter with a unique code and instructions on how to validate your account."}
-              end
-              if new_need_attrs = session.delete(:new_need_attributes)
-                @user.needs.create(new_need_attrs)
-                flash[:notice] = "Congratulations! You have added your first request. Once we've validated your account then neighbours can help you."
-                redirect_to(page_path(Page.find_by_slug(:what_next))) and return
-              elsif session.delete(:redirect_to_needs)
-                flash[:notice] = "Congratulations! You've just registered. Once we've validated your account you'll be able to help your neighbours."
-                redirect_to(page_path(Page.find_by_slug(:what_next))) and return
-              else
-                redirect_to(page_path(Page.find_by_slug(:what_next))) and return
-              end
-            else
-              flash[:notice] = "Congratulations! You've just registered. We'll contact you once Neighbours Can Help is available in your area."
-              redirect_to(page_path(Page.find_by_slug(:what_next))) and return
+            if @user.validate_by == "post" && !@user.validated?
+              flash[:modal] = {:title => "Thanks for registering", :text => "We'll send you a letter with a unique code and instructions on how to validate your account."}
             end
+            if new_need_attrs = session.delete(:new_need_attributes)
+              @user.needs.create(new_need_attrs)
+              flash[:notice] = "Congratulations! You have added your first request. Once we've validated your account then neighbours can help you."
+            elsif session.delete(:redirect_to_needs)
+              flash[:notice] = "Congratulations! You've just registered. Once we've validated your account you'll be able to help your neighbours."
+            end
+            redirect_to(Page.find_by_slug(:what_next) || root_path) and return
           else
-            flash[:error] = "Something's gone wrong - sorry.  Please try again"
-            redirect_to new_registration_path and return
+            flash[:error] = "Something's gone wrong - sorry. Please try again"
+            redirect_to(new_registration_path) and return
           end
         else
           @user.next_step!
