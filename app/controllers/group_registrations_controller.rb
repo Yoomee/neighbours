6a91f86614fc945c9a65e3be9d699fa8e6888aca
Ian Mooney <ian@yoomee.com>
2013-06-05 14:52:25 +0100

    Fixes and styles for group_registrations#new

--- app/controllers/group_registrations_controller.rb
+++ app/controllers/group_registrations_controller.rb
@@ -6,10 +6,6 @@ class GroupRegistrationsController < ApplicationController
 
   def create
     @user = User.new(params[:user].merge(:role => 'group_user'))
-    if params[:user][:encrypted_password].present?
-      @user.password = 'ignore-this'
-      @user.encrypted_password = params[:user][:encrypted_password]
-    end
     if @user.save
       sign_in(@user)
       flash[:notice] = 'Thanks for registering!'
