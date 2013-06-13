d69219082bab669e74d8965300c8013aceb27c77
Ian Mooney <ian@yoomee.com>
2013-06-13 12:29:03 +0100

    Show full_name on users#index

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,7 +1,7 @@
 module AdminHelper
   
-  def name_and_role(user)
-    user.full_name.tap do |out|
+  def full_name_and_role(user)
+    link_to(user.full_name.titleize, user).tap do |out|
       if user.role == 'admin'
         out << "&nbsp;" + content_tag(:span, 'Admin', :class => "badge badge-square badge-info")
       end
