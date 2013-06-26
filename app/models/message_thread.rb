b3df987c8706aaff5e7acb17552c7d5301fc96f4
Edward Andrews-Hodgson <edward@yoomee.com>
2013-06-26 14:59:30 +0100

    Flag messages as inappropriate

--- app/models/message_thread.rb
+++ app/models/message_thread.rb
@@ -2,4 +2,10 @@ class MessageThread < ActiveRecord::Base
 
   include YmMessages::MessageThread
 
+  has_many :flags, :as => :resource, :dependent => :destroy
+
+  def to_s
+    "messages between #{users.collect(&:full_name).to_sentence}"
+  end
+
 end
