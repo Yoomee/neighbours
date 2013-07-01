6b002db6483a7a983f5236f531c534d5fa07f958
Edward Andrews-Hodgson <edward@yoomee.com>
2013-07-01 16:21:49 +0100

    Group users can see other group users photos

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,10 +1,14 @@
 module ImageHelper
   def image_for_with_validation(object, geo_string, options = {})
-    if current_user.try(:validated?)
+    if current_user.try(:validated?) || object_is_a_user_in_the_same_group_as_current_user?(object)
       image_for(object, geo_string, options)
     else
       dragonfly_image_tag(object.default_image, geo_string, options)
     end
+  end
+
+  def object_is_a_user_in_the_same_group_as_current_user?(object)
+    current_user && object.is_a?(User) && (object.groups & current_user.groups).present?
   end
   
 end
