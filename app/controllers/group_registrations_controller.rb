437bc0eaf7d51dff1c713eca0e3a6d7dbb44150f
Edward Andrews-Hodgson <edward@yoomee.com>
2013-07-03 10:35:18 +0100

    Create group user from preregistered user

--- app/controllers/group_registrations_controller.rb
+++ app/controllers/group_registrations_controller.rb
@@ -8,9 +8,14 @@ class GroupRegistrationsController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user].merge(:role => 'group_user'))
-    @user.owned_groups.each {|g| g.owner = @user}
+    debugger
+    if @user = User.find_by_id(session[:pre_register_user_id])
+      @user.attributes = (params[:user]).merge(:role => 'group_user')
+    else
+      @user = User.new(params[:user].merge(:role => 'group_user'))
+    end
     if @user.in_group_and_user_creation?
+      @user.owned_groups.each {|g| g.owner = @user}
       if @user.current_group_step_group?
         @user.owned_groups.each {|g| g.owner = @user}
         if @user.owned_groups.first.valid?
@@ -31,6 +36,10 @@ class GroupRegistrationsController < ApplicationController
             return_or_redirect_to groups_path
           end
         else
+          if @user.errors[:email] == ["is already taken.  <a href='/login'>Log in</a> if this is your account"] && User.find_by_email(@user.email).role == 'pre_registration'
+            @already_pre_registered = true
+            UserMailer.complete_group_registration(User.find_by_email(@user.email)).deliver
+          end
           render :template => 'groups/new'
         end
       else
@@ -49,8 +58,7 @@ class GroupRegistrationsController < ApplicationController
       else
         if @user.errors[:email] == ["is already taken.  <a href='/login'>Log in</a> if this is your account"] && User.find_by_email(@user.email).role == 'pre_registration'
           @already_pre_registered = true
-          @user.ensure_authentication_token
-          UserMailer.complete_group_registration(@user).deliver
+          UserMailer.complete_group_registration(User.find_by_email(@user.email)).deliver
         end
         render :action => 'new'
       end
