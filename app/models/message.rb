d15decdb3a823bb2cd9e9e2c46dcd9034e73b6f8
Ian Mooney <ian@yoomee.com>
2013-07-05 11:34:15 +0100

    Allow admins to send anyone a message

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,6 +5,7 @@ class Message < ActiveRecord::Base
   class << self
     
     def valid_recipients_for_user(user)
+      return User.without(user).not_deleted if user.admin?
       return [] if user.no_private_messaging?
       User.without(user).joins(:groups).where('no_private_messaging = 0 AND groups.id IN (?)', user.group_ids).group('users.id')
     end
