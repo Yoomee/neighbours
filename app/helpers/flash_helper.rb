6986c44c8303beb095b47971212962f6e87cb5bc
Chrystal Griffiths <chrystal@yoomee.com>
2013-11-13 16:32:51 +0000

    Show 'Complete registration' link in flash

---
+++ app/helpers/flash_helper.rb
@@ -0,0 +1,18 @@
+module FlashHelper
+  
+  include YmCore::FlashHelper
+
+  def render_flash
+    flash.map do |key, val|
+      if key == :modal
+        render_modal(val)
+      else
+        if val == "Welcome back!" && current_user && !current_user.fully_registered? && current_user.neighbourhood
+          val << " You haven't fully registered yet. #{link_to "Complete your registration", new_registration_path, :class => "no-color underline"}"
+        end
+        content_tag(:div, dismiss_link('x','alert') + val.html_safe, :class => alert_class(key))
+      end
+    end.join.html_safe
+  end
+
+end
