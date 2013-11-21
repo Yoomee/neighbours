d81b19ec91eac59d086aca53b099223f25e85002
Matt Atkins <matt@yoomee.com>
2013-11-21 12:34:36 +0000

    User search, differentiate tabs with no results. Index last name

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -8,4 +8,15 @@ module AdminHelper
     end.html_safe
   end
   
+  def users_tab_li(name, users)
+    li_class = users.size.zero? ? 'no-results' : 'results'
+    if @active_tab.blank? && users.size.nonzero?
+      @active_tab = name
+      active_class = 'active'
+    end
+    content_tag(:li, :class => li_class) do
+      link_to(name, "##{name.downcase.gsub(' ', '-')}", :'data-toggle' => 'tab')
+    end
+  end
+  
 end
