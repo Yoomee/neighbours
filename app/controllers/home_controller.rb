4387312a3cbd76e8e8d5ed60bb08d787e06bb283
Chrystal Griffiths <chrystal@yoomee.com>
2013-12-10 10:29:14 +0000

    Fix removed_at selection for cycled column arrays

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -24,12 +24,12 @@ class HomeController < ApplicationController
   def get_items_for_cycle_columns
     lat, lng = Need.get_lat_lng_from_postcode(params[:postcode]) if params[:postcode]
     if current_user.try(:has_lat_lng?)
-      @general_offers = GeneralOffer.from_live_neighbourhood.visible_from_location(current_user.lat, current_user.lng, :order_by_closest => true, :limit => 20).where(:removed_at => nil).at_least(20)
+      @general_offers = GeneralOffer.from_live_neighbourhood.visible_from_location(current_user.lat, current_user.lng, :order_by_closest => true, :limit => 20).at_least(20).select{|need| need.removed_at.nil?}
       @helped = Need.closest_to(current_user, :with => {:neighbourhood_live => true, :resolved => true}, :limit => 20).at_least(20).select{|need| need.removed_at.nil?} if @general_offers.empty?
       @needs = Need.closest_to(current_user, :with => {:neighbourhood_live => true, :resolved => false}, :without => {:deadline => Need.first.created_at..Time.now}, :limit => 20).at_least(20).select{|need| need.removed_at.nil?}
     else
-      @general_offers = GeneralOffer.from_live_neighbourhood.visible_to_user(current_user).order('created_at DESC').where(:removed_at => nil).limit(20).at_least(20)
-      @helped = Need.from_live_neighbourhood.resolved.order('created_at DESC').limit(20).at_least(20).where(:removed_at => nil) if @general_offers.empty?
+      @general_offers = GeneralOffer.from_live_neighbourhood.visible_to_user(current_user).order('created_at DESC').limit(20).at_least(20).select{|need| need.removed_at.nil?}
+      @helped = Need.from_live_neighbourhood.resolved.order('created_at DESC').limit(20).at_least(20).select{|need| need.removed_at.nil?} if @general_offers.empty?
       @needs = Need.from_live_neighbourhood.unresolved.deadline_in_future.order('created_at DESC').limit(20).at_least(20).select{|need| need.removed_at.nil?}
     end
   end
