2f8eb4fe2d1da98fe6a441d9d1f921ad22dc1b9e
Chrystal Griffiths <chrystal@yoomee.com>
2014-01-14 09:51:52 +0000

    Change get_thumbnail_url to not be a class method

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,26 +5,26 @@ class Page < ActiveRecord::Base
   has_slideshow
 
   class << self
-    
+
     def view_names
       %w{basic tiled list about_view}
     end
 
-    def get_thumbnail_url(text)
-      video_url = extract_video_url_from_text(text)
-      video_id = video_url[video_url.rindex("/") + 1..video_url.length]
-      response = JSON.parse(open("http://vimeo.com/api/v2/video/#{video_id}.json").read)
-      response.first["thumbnail_medium"]
-    end
+  end
 
-    private
+  def get_thumbnail_url
+    video_url = extract_video_url_from_text(text)
+    video_id = video_url[video_url.rindex("/") + 1..video_url.length]
+    response = JSON.parse(open("http://vimeo.com/api/v2/video/#{video_id}.json").read)
+    response.first["thumbnail_medium"]
+  end
 
-    def extract_video_url_from_text(string)
-      substring = "http://player.vimeo.com/video/"
-      url_start_index = string.index(substring)
-      string[url_start_index..string.index('"', url_start_index)].chop
-    end
-    
+  private
+
+  def extract_video_url_from_text(string)
+    substring = "http://player.vimeo.com/video/"
+    url_start_index = string.index(substring)
+    string[url_start_index..string.index('"', url_start_index)].chop
   end
   
 end
