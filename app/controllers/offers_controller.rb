60b3bbb47bae151e82949cd12bd2e284f3817643
Greg Woodcock <greg@yoomee.com>
2014-01-17 16:13:16 +0000

    Allow the destroying of already deleted offers

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -14,7 +14,7 @@ class OffersController < ApplicationController
   end
 
   def destroy_all
-    Offer.destroy_all(['offers.id IN (?)', params[:ids]])
+    Offer.unscoped.destroy_all(['offers.id IN (?)', params[:ids]])
     flash[:notice] = "Offers successfully destroyed."
     render :nothing => true
   end
@@ -23,11 +23,12 @@ class OffersController < ApplicationController
     Offer.where('offers.id IN (?)', params[:ids]).each do |offer|
       unless offer.removed_at.present?
         offer.update_attribute(:removed_at, Time.now)
+      end
     end
     flash[:notice] = "Offers successfully deleted."
     render :nothing => true
   end
-  
+
   def accept
     @offer.update_attribute(:accepted, true)
     UserMailer.accepted_offer(@offer).deliver
@@ -41,7 +42,7 @@ class OffersController < ApplicationController
     flash[:notice] = "Your offer has been withdrawn."
     redirect_to @offer.need
   end
-  
+
   def reject
     @offer.update_attribute(:accepted, false)
     if @offer.need.posts.first.comments.first
