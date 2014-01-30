d347728b797b8496f922a39ed766b33d55109ffd
Chrystal Griffiths <chrystal@yoomee.com>
2014-01-30 16:12:44 +0000

    Fix general offers of deleted users showing up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
       needs = Need.unresolved.with_lat_lng.visible_to_user(current_user)
       @needs_json = needs.to_json(:current_user => current_user, :only => [:id], :methods => [:lat, :lng, :miles_from_s, :title, :user_first_name])
       @users_json = User.visible_to_user(current_user).to_json(:current_user => current_user, :only => [:id, :lat, :lng, :first_name], :methods => [:miles_from_s])
-      @general_offers_json = GeneralOffer.from_live_neighbourhood.visible_to_user(current_user).to_json(:current_user => current_user, :only => [:id], :methods => [:lat, :lng, :miles_from_s, :title, :user_first_name])
+      @general_offers_json = GeneralOffer.where('users.is_deleted IS FALSE').from_live_neighbourhood.visible_to_user(current_user).to_json(:current_user => current_user, :only => [:id], :methods => [:lat, :lng, :miles_from_s, :title, :user_first_name])
     else
       @needs_json ||= []
       @users_json ||= []
@@ -26,11 +26,11 @@ class HomeController < ApplicationController
   def get_items_for_cycle_columns
     lat, lng = Need.get_lat_lng_from_postcode(params[:postcode]) if params[:postcode]
     if current_user.try(:has_lat_lng?)
-      @general_offers = GeneralOffer.from_live_neighbourhood.visible_from_location(current_user.lat, current_user.lng, :order_by_closest => true, :limit => 20).at_least(20).select{|need| need.removed_at.nil?}
+      @general_offers = GeneralOffer.joins(:user).where('users.is_deleted IS FALSE').from_live_neighbourhood.visible_from_location(current_user.lat, current_user.lng, :order_by_closest => true, :limit => 20).at_least(20).select{|need| need.removed_at.nil?}
       @helped = Need.resolved.closest_to(current_user, :with => {:neighbourhood_live => true, :resolved => true}, :limit => 20).at_least(20).select{|need| need.removed_at.nil?} if @general_offers.empty?
       @needs = Need.unresolved.closest_to(current_user, :with => {:neighbourhood_live => true, :resolved => false}, :without => {:deadline => Need.first.created_at..Time.now}, :limit => 20).at_least(20).select{|need| need.removed_at.nil?}
     else
-      @general_offers = GeneralOffer.from_live_neighbourhood.visible_to_user(current_user).order('created_at DESC').limit(20).at_least(20).select{|need| need.removed_at.nil?}
+      @general_offers = GeneralOffer.where('users.is_deleted IS FALSE').from_live_neighbourhood.visible_to_user(current_user).order('created_at DESC').limit(20).at_least(20).select{|need| need.removed_at.nil?}
       @helped = Need.from_live_neighbourhood.resolved.order('created_at DESC').limit(20).at_least(20).select{|need| need.removed_at.nil?} if @general_offers.empty?
       @needs = Need.from_live_neighbourhood.unresolved.deadline_in_future.order('created_at DESC').limit(20).at_least(20).select{|need| need.removed_at.nil?}
     end
