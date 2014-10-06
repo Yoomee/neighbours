251d4d37d3236a42e5d552cebae4a042690da470
Edward Andrews-Hodgson <edward@yoomee.com>
2014-10-06 11:26:38 +0100

    Reverse honeypot
    Try to stop spam

--- app/controllers/enquiries_controller.rb
+++ app/controllers/enquiries_controller.rb
@@ -1,8 +1,22 @@
 class EnquiriesController < ApplicationController
-  
+
   include YmEnquiries::EnquiriesController
   load_and_authorize_resource
-  
+
+  def create
+    # debugger
+    redirect_to root_url and return if params[:extra_message].present?
+    @enquiry = Enquiry.new(params[:enquiry].merge(:form_name => params[:id]))
+    if @enquiry.save
+      YmEnquiries::EnquiryMailer.new_enquiry(@enquiry).deliver
+      flash[:notice] = "#{@enquiry.response_message}"
+      redirect_to root_url
+    else
+      render :action => 'new'
+    end
+  end
+
+
   def new
     if params[:id].nil?
       render_404
@@ -12,7 +26,7 @@ class EnquiriesController < ApplicationController
         if attrs[:form_name] == "feedback" && current_user
           attrs.merge!(current_user.attributes.slice("first_name", "last_name", "email"))
         elsif attrs[:form_name] == "other_neighbourhood" && current_user
-          attrs.merge!(current_user.attributes.slice("first_name", "last_name", "email")) 
+          attrs.merge!(current_user.attributes.slice("first_name", "last_name", "email"))
         end
         @enquiry = Enquiry.new(attrs)
       rescue NameError
@@ -20,5 +34,5 @@ class EnquiriesController < ApplicationController
       end
     end
   end
-  
+
 end
